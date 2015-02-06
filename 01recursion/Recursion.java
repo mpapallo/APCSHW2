e76cb04018f0df36ee28f0e043859058b7435e0d
Michaela Papallo <michaelapapallo@gmail.com>
2015-02-05 21:50:54 -0500

    made the file

---
+++ 01recursion/Recursion.java
@@ -0,0 +1,19 @@
+public class Recursion{
+    
+    public String name(){
+	return "Papallo,Michaela";
+    }
+
+    public int fact(int n){
+	
+    }
+
+    public int fib(int n){
+	
+    }
+
+    public double sqrt(double n){
+	
+    }
+
+}
