46d0efc7b1b7c33c03380d7ac4983236492554ef
Michaela Papallo <michaelapapallo@gmail.com>
2015-02-06 20:48:54 -0500

    fact and fib

---
+++ 01recursion/Driver.java
@@ -0,0 +1,18 @@
+public class Driver{
+    public static void main(String[]args){
+	Recursion r = new Recursion();
+
+	System.out.println(r.name());
+
+	System.out.println(r.fact(0));
+	System.out.println(r.fact(1));
+	System.out.println(r.fact(2));
+	System.out.println(r.fact(5));
+	System.out.println("^should be 120");
+
+	System.out.println(r.fib(0));
+	System.out.println(r.fib(1));
+	System.out.println(r.fib(5));
+	System.out.println("^ should be 8");
+    }
+}
--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -5,15 +5,33 @@ public class Recursion{
     }
 
     public int fact(int n){
-	
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	if (n == 0){
+	    return 1;
+	}
+	return n * fact(n - 1);
     }
 
     public int fib(int n){
-	
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	if (n == 0){
+	    return 1;
+	}
+	return fibHelper(n, 1, 1);
     }
 
-    public double sqrt(double n){
-	
+    public int fibHelper(int n, int a, int b){
+	if (n == 1){
+	    return b;
+	}
+	return fibHelper(n - 1, b, b + a);
     }
 
+    //public double sqrt(double n){
+	
+    //}
 }
