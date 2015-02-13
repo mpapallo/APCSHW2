00e09ff11ecd3c0a6a385c24ceaf671e30168b9e
mpapallo <michaelapapallo@gmail.com>
2015-02-13 12:10:18 -0500

    works and is very fast for 5-7

--- 02knights/Driver.java
+++ 02knights/Driver.java
@@ -1,6 +1,7 @@
 public class Driver{
     public static void main(String[] args){
-	KnightsTour tour = new KnightsTour(5);
+	KnightsTour tour = new KnightsTour(7);
 	tour.solve();
+	System.out.println(tour);
     }
 }
--- 02knights/KnightsTour.java
+++ 02knights/KnightsTour.java
@@ -3,13 +3,13 @@ import java.io.*;
 
 public class KnightsTour{
     //constants for the class
+
     //terminal specific character to clear screen , or hide/show cursor
     final static String clear =  "\033[2J";
     final static String hide =  "\033[?25l";
     final static String show =  "\033[?25h";
     //instance variable
     private int[][]board;
-
     //terminal specific character to move the cursor
     private String go(int x,int y){
 	return ("\033[" + x + ";" + y + "H");
@@ -56,27 +56,28 @@ public class KnightsTour{
     }
 
     public boolean solve(int startx, int starty){
-	return solve(startx, starty, 0);
+	return solve(startx, starty, 1);
     }
 		
     public boolean solve(int x,int y,int currentMoveNumber){
-	System.out.println(this);
-	wait(5);
+	//System.out.println(this);
+	//wait(5);
 	if (y < 0 || x < 0 || y >= board.length || x >= board[0].length){
 	    return false;
 	}
-	if (board[y][x] == 0 && board.length * board.length == currentMoveNumber - 1){
+	if (board[y][x] == 0 && board.length * board.length == currentMoveNumber){
+	    board[y][x] = currentMoveNumber;
 	    return true;
 	}
 	if (board[y][x] == 0){
 	    board[y][x] = currentMoveNumber;
-	    if (solve(x + 2, y + 1, currentMoveNumber + 1) ||
+	    if (solve(x + 2, y - 1, currentMoveNumber + 1) ||
+		solve(x + 1, y - 2, currentMoveNumber + 1) ||
+		solve(x + 2, y + 1, currentMoveNumber + 1) ||
 		solve(x - 2, y + 1, currentMoveNumber + 1) ||
-		solve(x + 2, y - 1, currentMoveNumber + 1) ||
 		solve(x - 2, y - 1, currentMoveNumber + 1) ||
 		solve(x + 1, y + 2, currentMoveNumber + 1) ||
 		solve(x - 1, y + 2, currentMoveNumber + 1) ||
-		solve(x + 1, y - 2, currentMoveNumber + 1) ||
 		solve(x - 1, y - 2, currentMoveNumber + 1)){
 		return true;
 	    }
