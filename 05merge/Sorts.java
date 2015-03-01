ed335fe3eab64a50c922b483ce3038cbe98d354d
Michaela Papallo <michaelapapallo@gmail.com>
2015-02-28 22:55:18 -0500

    working on merge

--- 05merge/Sorts.java
+++ 05merge/Sorts.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.io.*;
 public class Sorts{
 
     public static int[] merge(int[] a, int[] b){
@@ -26,15 +28,41 @@ public class Sorts{
     }
 
     public static void mergesort(int[] m){
-	
+        m = msh(m);
+    }
+
+    public static int[] msh(int[] m){
+	if (m.length == 1){
+	    return m;
+	}
+	int[] first = new int[m.length / 2];
+	int[] second = new int[m.length - first.length];
+	for (int i = 0; i < first.length; i ++){
+	    first[i] = m[i];
+	}
+	for (int i = 0; i < second.length; i ++){
+	    second[i] = m[first.length + i];
+	}
+	return merge(msh(first), msh(second));
     }
 
     public static void main(String[] args){
+	/*
 	int[] a = {1, 3, 5, 7, 7, 9, 20};
 	int[] b = {1, 2, 4, 6, 8, 56};
 	int[]c = merge(a, b);
         for (int x = 0; x < c.length; x ++){
 	    System.out.print(c[x] + ",");
+	}
+	*/
+	System.out.println();
+	int[] test = new int[10];
+	for (int i = 0; i < 10; i ++){
+	    test[i] = 10 - i;
+	}
+	mergesort(test);
+	for (int i = 0; i < test.length; i ++){
+	    System.out.print(test[i] + ",");
 	}
     }
 
