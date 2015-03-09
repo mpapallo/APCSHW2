3b21c6ccb699c687ce9aa276a69b825755246712
mpapallo <michaelapapallo@gmail.com>
2015-03-09 12:19:48 -0400

    trying to make it partition in place

--- 06quick/QuickSelect.java
+++ 06quick/QuickSelect.java
@@ -4,7 +4,40 @@ public class QuickSelect{
     private static Random rand = new Random();
 
     public static int select(int[] ary, int n){
-	return partition(ary, n, 0, ary.length - 1);
+	return partitionInPlace(ary, n, 0, ary.length - 1);
+    }
+
+    public static int partitionInPlace(int[] ary, int n, int si, int ei){
+	//randomly pick a pivot point, save its value
+	int ri = rand.nextInt(ei + 1 - si) + si;
+	int pivot = ary[ri];
+	int start = si;
+	int end = ei;
+	//for elements between si and ei:
+	for (int i = si; i <= ei; i ++){
+	    if (ary[i] < pivot){
+		int x = ary[start];
+		ary[start] = ary[i];
+		ary[i] = ary[start];
+		start ++;
+	    }else if (ary[i] > pivot){
+	        int x = ary[end];
+		ary[end] = ary[i];
+		ary[i] = x;
+		end --;
+	    }
+	}
+	ary[start] = pivot;
+	System.out.println(pivot);
+	System.out.println(Arrays.toString(ary));
+	if (start == n - 1){
+	    return ary[start];
+	}
+	else if(start < n - 1){
+	    return partitionInPlace(ary, n, start, ei);
+	}else{
+	    return partitionInPlace(ary, n, si, start);
+	}
     }
 
     public static int partition(int[] ary, int n, int si, int ei){
