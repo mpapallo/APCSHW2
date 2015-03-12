3970a68d666b70076ce64378c59c08ed9d1570e9
mpapallo <michaelapapallo@gmail.com>
2015-03-12 11:57:11 -0400

    moved main out of driver

--- 07LinkedList/MyLinkedList.java
+++ 07LinkedList/MyLinkedList.java
@@ -11,7 +11,7 @@ public class MyLinkedList{
     }
 
     public String toString(){
-	String list = "[";
+	String list = "[ ";
 	now = head;
 	for (int i = 0; i < size; i ++){
 	    list += now.getData() + " ";
@@ -97,6 +97,30 @@ public class MyLinkedList{
 
     public void clear(){
 	size = 0;
+    }
+
+    public static void main(String[] args){
+	MyLinkedList l = new MyLinkedList();
+	
+	System.out.println(l);
+	
+	l.add(3);
+	l.add(4);
+	l.add(5);
+
+	System.out.println(l);
+	
+	System.out.println("get(0) " + l.get(0));
+	System.out.println("get(2) " + l.get(2));
+
+	l.set(2, 6);
+	System.out.println("set(2, 6) " + l);
+
+	System.out.println("indexOf(6) " + l.indexOf(6));
+	
+	l.add(1, 9);
+	System.out.println("add(1, 9) "  + l);
+
     }
 
 }
