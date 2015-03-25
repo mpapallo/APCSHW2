b4965be7fb3f66dcb3de0512cda89344b3e66764
mpapallo <michaelapapallo@gmail.com>
2015-03-25 12:00:30 -0400

    framework

---
+++ 10DEQ/MyDeque.java
@@ -0,0 +1,28 @@
+public class MyDEQ<T>{
+
+    private Object[] deq;
+    private int head;
+    private int tail;  
+    private int size;
+
+    public MyDEQ(){
+	deq = new Object[100];
+	head = 50;
+	tail = 50;
+	size = 0;
+    }
+
+    public void addFirst(T value){
+	
+    }
+    public void addLast(T value){
+
+    }
+
+    public T removeFirst(){
+	
+    }
+    public T removeLast(){
+	
+    }
+}
