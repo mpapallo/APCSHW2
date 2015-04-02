d589adb3085fe27899419ed1eadbde690a09c7aa
mpapallo <michaelapapallo@gmail.com>
2015-04-02 12:23:45 -0400

    def doesnt work, but starting on track

--- 11BFS/Maze.java
+++ 11BFS/Maze.java
@@ -6,6 +6,7 @@ public class Maze{
     private int maxx, maxy;
     private int startx,starty;
     private Frontier solver;
+    private Coord end;
 
     public Maze(String filename){
 	startx = -1;
@@ -13,8 +14,6 @@ public class Maze{
 	String ans = "";
 	try{
 	    Scanner in = new Scanner(new File(filename));
-
-	    //keep reading next line
 	    while(in.hasNext()){
 		String line= in.nextLine();
 		if(maxy==0){
@@ -45,33 +44,21 @@ public class Maze{
 
     private String go(int x,int y){
 	return ("["+x+";"+y+"H");
-    }
-    
+    }    
     private String clear(){
 	return  "[2J";
     }
-
     private String hide(){
 	return  "[?25l";
     }
-
     private String show(){
 	return  "[?25h";
     }
     private String invert(){
 	return  "[37";
     }
-
     public void clearTerminal(){
 	System.out.println(clear());
-    }
-
-    public void wait(int millis){
-	try {
-	    Thread.sleep(millis);
-	}
-	catch (InterruptedException e) {
-	}
     }
 
     public String toString(){
@@ -99,25 +86,67 @@ public class Maze{
 	}
     }
 
+    public void wait(int millis){
+	try {
+	    Thread.sleep(millis);
+	}
+	catch (InterruptedException e) {
+	}
+    }
+
     public boolean solveBFS(){
 	return solveBFS(false);
     }
-    public boolean solveDFS(){
-	return solveDFS(false);
-    }
     public boolean solveBFS(boolean animate){
 	solver = new Frontier(1);
-	return BFS(solver, startx, starty);
+	Coord c = new Coord(startx, starty);
+	return BFS(solver, c);
+    }    
+    public boolean BFS(Frontier f, Coord current){
+	f.add(current);
+	if (inRange(x, y) && isValidSpace(x, y)){
+	    if (maze[y][x] == 'E'){
+		end = current;
+		return true;
+	    }else if (maze[y][x] == 'S' || maze[y][x] == ' '){
+		maze[y][x] = 'X';
+
+		Coord a = new Coord(x-1, y);
+		a.setPrev(current);
+		Coord b = new Coord(x+1, y);
+		b.setPrev(current);
+		Coord c = new Coord(x, y-1);
+		c.setPrev(current);
+		Coord d = new Coord(x, y+1);
+		d.setPrev(current);
+		f.add(a);
+		f.add(b);
+		f.add(c);
+		f.add(d);
+		
+		return BFS(f, f.remove()); 
+	    }
+	}else{
+	    return false;
+	}
+    }
+    /*
+    public boolean solveDFS(){
+	return solveDFS(false);
     }
     public boolean solveDFS(boolean animate){
 	solver = new Frontier(0);
 	return DFS(solver, startx, starty);
     }
-    public boolean BFS(Frontier f, int x, int y){
+    public boolean DFS(Frontier f, int x, int y){
 	
     }
-    public boolean DFS(Frontier f, int x, int y){
-
+    */
+    public boolean inRange(int x, int y){
+	return !(x < 0 || y < 0 || x > maxx || y > maxy);
+    }
+    public boolean isValidSpace(int x, int y){
+	return !(maze[y][x] == '#');
     }
 
     public static void main(String[]args){
