3eff3ac93ef2940110505b0a5267839e56ade35a
mpapallo <michaelapapallo@gmail.com>
2015-04-23 12:07:28 -0400

    driver

--- 12Trees/BTree.java
+++ 12Trees/BTree.java
@@ -1,12 +1,18 @@
 import java.util.*;
 import java.io.*;
+
 public class BTree<T>{
 
     public static final int PRE_ORDER = 0;
     public static final int IN_ORDER = 1;
     public static final int POST_ORDER = 2;
-
+    private Random rand;
     private TreeNode<T> root;
+
+    public BTree(){
+	root = null;
+	rand = new Random();
+    }
 
     public void add(T d){
 	TreeNode<T> a = new TreeNode<T>(d);
@@ -20,7 +26,7 @@ public class BTree<T>{
 	}else if (curr.getRight() == null){
 	    curr.getRight();
 	}else{
-	    int x = Math.random(2);
+	    int x = rand.nextInt(2);
 	    if (x == 0){
 		add(curr.getLeft(), bn);
 	    }else{
