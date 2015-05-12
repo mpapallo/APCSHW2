b2a6bc0ff0d728651875cafc1df834028fd3de02
mpapallo <michaelapapallo@gmail.com>
2015-05-12 12:16:37 -0400

    started add; it compiles tho

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,15 +1,48 @@
 public class MyHeap{
 
-    private ArrayList<Integer> heap;
+    private int[] heap;
+    private boolean isMax;
 
     public MyHeap(){
-	heap = new ArrayList<Integer>();
+	this(true);
+    }
+    public MyHeap(boolean isMax){
+	heap = new int[10];
+	this.isMax = isMax;
     }
     
-    public void add(int){
-	
+    public String name(){
+	return "papallo.michaela";
     }
+
+    private void swap(int a, int b){
+	//assumes a and b are in range
+	int temp = heap[a];
+	heap[a] = heap[b];
+	heap[b] = temp;
+    }
+
+    public void add(int n){
+
+	//////if heap[0]  = heap.length - 1, resize
+
+	int size = heap[0] + 1; //new size
+	heap[size] = n; //add int to the end of the values
+	heap[0] = size; //update size
+	int i = size;
+	while (i / 2 > 0){ //while it has a parent
+	    if (heap[i] > heap[i / 2]){
+		//if it is greater than its parent, swap them (maxHeap)
+		swap(i, i / 2);
+	    }
+	}
+    }
+
     public int remove(){
+	return 0;
+    }
+
+    public int peek(){
 	return 0;
     }
 
