3189edb2dfa9eb15090eb15c7911f4a081585ad1
Michaela Papallo <michaelapapallo@gmail.com>
2015-05-11 21:16:58 -0400

    frame for myHeap

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,21 @@
+public class MyHeap{
+
+    private ArrayList<Integer> heap;
+
+    public MyHeap(){
+	heap = new ArrayList<Integer>();
+    }
+    
+    public void add(int){
+	
+    }
+    public int remove(){
+	return 0;
+    }
+
+    public String toString(){
+	String ret = "";
+	return ret;
+    }
+
+}
