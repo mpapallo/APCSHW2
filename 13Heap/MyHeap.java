7df9235575478d142486732f713e9826e202f33b
Michaela Papallo <michaelapapallo@gmail.com>
2015-05-13 22:19:27 -0400

    remove works i think

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MyHeap{
 
     private int[] heap;
@@ -7,7 +8,7 @@ public class MyHeap{
 	this(true);
     }
     public MyHeap(boolean isMax){
-	heap = new int[10];
+	heap = new int[100];
 	this.isMax = isMax;
     }
     
@@ -15,11 +16,21 @@ public class MyHeap{
 	return "papallo.michaela";
     }
 
-    public void swap(int a, int b){
+    private void swap(int a, int b){
 	//assumes a and b are in range
 	int temp = heap[a];
 	heap[a] = heap[b];
 	heap[b] = temp;
+    }
+
+    private int getLeft(int i){
+	return i * 2;
+    }
+    private int getRight(int i){
+	return i * 2 + 1;
+    }
+    private int getParent(int i){
+	return i / 2;
     }
 
     public void add(int n){
@@ -30,27 +41,51 @@ public class MyHeap{
 	heap[size] = n; //add int to the end of the values
 	heap[0] = size; //update size
 	int i = size;
-	while (i / 2 > 0){ //while it has a parent
-	    if (heap[i] > heap[i / 2]){
+	while (getParent(i) > 0){ //while it has a parent
+	    int parentIn = getParent(i);
+	    if (heap[i] > heap[parentIn]){
 		//if it is greater than its parent, swap them (maxHeap)
-		swap(i, i / 2);
+		swap(i, parentIn);
 	    }
-	    i = i /2;
+	    i = parentIn;
 	}
     }
 
     public int remove(){
 	int size = heap[0];
 	if (size == 0){
-	    //?
+	    throw new NoSuchElementException();
 	}else{
 	    int ret = heap[1];
 	    heap[1] = heap[size]; //replace root with last value
-	    heap[0] = size - 1; //reduce size
+	    heap[0] = --size; //reduce size
 	    //do the swap down thing
-	   
-	    
-
+	    int i = 1;
+	    while (getLeft(i) <= size){ //while value has a left child
+		int l = getLeft(i);
+		int r = getRight(i);
+	        if (r <= size){ //if value has a right child
+		    if (heap[i] < heap[l] || heap[i] < heap[r]){
+			//if value is less than one of its children,
+			//swap with the bigger one (maxHeap)
+			int dif = heap[l] - heap[r];
+			if (dif > 0){
+			    swap(i, l);
+			    i = l;
+			}else if (dif < 0){
+			    swap(i, r);
+			    i = r;
+			}
+		    }else{
+			return ret;
+		    }
+		}else if (heap[i] < heap[l]){
+		    swap(i, l);
+		    i = l;
+		}else{
+		    return ret;
+		}
+	    }
 	    return ret;
 	}
 	
@@ -58,7 +93,7 @@ public class MyHeap{
 
     public int peek(){
 	if (heap[0] == 0){
-	    //?
+	    throw new NoSuchElementException();
 	}else{
 	    return heap[1];
 	}
@@ -84,6 +119,12 @@ public class MyHeap{
 	h.add(3);
 	h.add(1);
 	h.add(5);
+	h.add(4);
+	h.add(7);
+	h.add(9);
+	h.add(1);
+	System.out.println(h);
+	System.out.println(h.remove());
 	System.out.println(h);
 	
     }
