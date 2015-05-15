99691d457f10c57d7c78fad8e9f9cf569acd387b
Michaela Papallo <michaelapapallo@gmail.com>
2015-05-14 21:19:13 -0400

    Running Median

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -1,5 +1,6 @@
 public class RunningMedian{
     
+    private MyHeap max, min;
     
     public RunningMedian(){
 	max = new MyHeap();
@@ -7,6 +8,7 @@ public class RunningMedian{
     }
 
     public double getMedian(){
+	return 0.0;
     }
     
     public void add(int n){
@@ -14,4 +16,3 @@ public class RunningMedian{
     }
     
 }
-
