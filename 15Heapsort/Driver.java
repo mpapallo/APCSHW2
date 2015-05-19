4126356644c209850f266242389045323a195802
Michaela Papallo <michaelapapallo@gmail.com>
2015-05-18 20:31:12 -0400

    sort almost works except for one swap...

--- 15Heapsort/Driver.java
+++ 15Heapsort/Driver.java
@@ -1,10 +1,12 @@
+import java.util.*;
 public class Driver{
 
     public static void main(String[]args){
 	
 	int[] arr = new int[] {8, 9, 3, 1, 6, 4, 2, 7};
-	
+	System.out.println(Arrays.toString(arr));
 	Sorts.heapsort(arr);
-
+	System.out.println(Arrays.toString(arr));
+    
     }
 }
--- 15Heapsort/Sorts.java
+++ 15Heapsort/Sorts.java
@@ -2,20 +2,26 @@ import java.util.*;
 public class Sorts{
     
     public static void heapsort(int[] arr){
-	System.out.println(Arrays.toString(arr));
+	//step one: heapify	
 	for (int i = arr.length - 1; i >= 0; i --){
-	    swapDown(arr, i);
+	    swapDown(arr, i, arr.length - 1);
 	}
 	System.out.println(Arrays.toString(arr));
+	//step two: sort
+	for (int len = arr.length - 1; len >= 0; len --){
+	    swap(arr, 0, len);
+	    System.out.println(Arrays.toString(arr));
+	    swapDown(arr, 0, len - 1);
+	    System.out.println(Arrays.toString(arr));
+	}
     }
 
-    private static void swapDown(int[] arr, int i){
-	int size = arr.length;
-	while (getLeft(i) < size){ //while value has a left child
+    private static void swapDown(int[] arr, int i, int size){
+	while (getLeft(i) <= size){ //while value has a left child
 	    int l = getLeft(i);
 	    int r = getRight(i);
 
-	    if (r < size){ //if value has a right child
+	    if (r <= size){ //if value has a right child
 		if (!inOrder(arr, i, l) || !inOrder(arr, i, l)){
 		    int dif = arr[l] - arr[r];
 		    if (dif > 0){
